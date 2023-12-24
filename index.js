91d6074e8a9e85892659ea256d0d6b83134faeb1
Madhan06 <63894524+madhan63@users.noreply.github.com>
2023-12-24 20:28:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+const jokeText=document.querySelector(".joke-text");
+const newJokeBtn=document.querySelector(".new-joke-btn");
+
+newJokeBtn.addEventListener("click",getJoke);
+getJoke();
+
+function getJoke(){
+    fetch("https://icanhazdadjoke.com/",{
+        headers:{
+            'Accept':'application/json'
+        }
+    }).then(function(response){
+        return response.json();
+    }).then(function(data){
+        const joke=data.joke;
+        jokeText.innerHTML=joke;
+    });
+}
+}
